9527f4f718c691e2621ebde2ea651d588fb45c19
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-07-06 21:55:24 +0300

    Create idealshouldercalc.py

---
+++ idealshouldercalc.py
@@ -0,0 +1,4 @@
+height =174; #enter your height value in cm
+waist = height*0.45; #This calculates the ideal waist value which 45% of your height
+shoulders = waist*1.6; #idealvalue for Shoulders using the ratio 1.6 waist
+print(shoulders); 
